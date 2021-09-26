e91f8738d96818b1aee34d9fb3fcda2039298ab1
Jean Dubois <jd-dev@laposte.net>
2021-09-26 12:05:40 +0200

    changement de la version

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from tkinter import messagebox
 from datetime import *
 import webbrowser
 
-__version__ = "4.0-rc2 (2021-08-31)"
+__version__ = "4.0 (2021-09-26)"
 __author__ = "Jean Dubois <jd-dev@laposte.net>"
 
 # création de la variable year (année)
